33730ffab0fdc814767c635a9a61708825f172c0
Derek <RumbleInCybertron@users.noreply.github.com>
2023-12-03 20:34:33 -0500

    + portfolio model init

--- src/Domain/Portfolios/Portfolio.cs
+++ src/Domain/Portfolios/Portfolio.cs
@@ -1,11 +1,13 @@
 using System.Data;
+using AssetIO.Domain;
+using Domain.StockAssets;
 
-namespace Domain.Models
+namespace Domain.Portfolios
 {
-  public class Portfolio
+  public class Portfolio : EntityBase
   {
     private string m_name = "";
-    private float m_ttlValue;
+    private float m_ttlValue = 0.0f;
     private string m_userId = "";
 
     public required string Name
@@ -16,7 +18,6 @@ namespace Domain.Models
         if (value == null) throw new NoNullAllowedException("Name cannot be null.");
         m_name = value;
       }
-
     }
     public required float Ttl_Value
     {
@@ -40,6 +41,7 @@ namespace Domain.Models
         m_userId = value;
       }
     }
+    public List<StockAsset> StockAssets { get; set; } = new List<StockAsset>();
   }
 
 }
