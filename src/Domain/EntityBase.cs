623a0d2c0fc942bd29afaddb095e4df7284c7a24
Derek <RumbleInCybertron@users.noreply.github.com>
2023-12-03 20:34:51 -0500

    + baes entity init

---
+++ src/Domain/EntityBase.cs
@@ -0,0 +1,19 @@
+using System.Data;
+
+namespace AssetIO.Domain
+{
+  public class EntityBase
+  {
+    private string m_id = "";
+
+    public string Id
+    {
+      get { return m_id; }
+      set
+      {
+        if (value == null) throw new NoNullAllowedException("Id cannot be null.");
+        m_id = value;
+      }
+    }
+  }
+}
